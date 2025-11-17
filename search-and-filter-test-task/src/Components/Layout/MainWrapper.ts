a8066340d61b073f99239c2352645a7b3fdba3ea
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2025-11-17 11:33:27 +0300

    displlay name filter

--- search-and-filter-test-task/src/Components/Layout/MainWrapper.ts
+++ search-and-filter-test-task/src/Components/Layout/MainWrapper.ts
@@ -1,13 +1,14 @@
 import Component from '../../component';
 import { updateSearchByFilter } from '../../helpers';
 import { createResultsWrapper } from '../Results';
-import createFilter from '../Search';
+import createTypeFilter, { createNameFilter } from '../Search';
 
 export default function createMainWrapper(): Component {
   updateSearchByFilter('');
   return new Component(
     { tag: 'main', className: 'main-wrapper' },
-    createFilter(),
+    createTypeFilter(),
+    createNameFilter(),
     new Component({ className: 'results-title', text: 'Results:' }),
     createResultsWrapper()
   );
--- search-and-filter-test-task/src/Components/Search.ts
+++ search-and-filter-test-task/src/Components/Search.ts
@@ -1,9 +1,9 @@
 import Component from '../component';
-import { updateSearchByFilter } from '../helpers';
+import { updateSearchByFilter, updateSearchByName } from '../helpers';
 
 const PART_TYPE_CODES = [565, 566];
 
-export default function createFilter(): Component {
+export default function createTypeFilter(): Component {
   const label = new Component({
     className: 'select-label',
     tag: 'label',
@@ -28,7 +28,39 @@ export default function createFilter(): Component {
 
     if (target instanceof HTMLSelectElement) {
       updateSearchByFilter(target.value);
+      clearNameSearch();
     }
   });
   return label;
 }
+
+export const createNameFilter = (): Component => {
+  const label = new Component({
+    className: 'name-input-label',
+    tag: 'label',
+    text: 'Name',
+  });
+  label.setAttribute('for', 'filter');
+  const input = new Component({ className: 'name-input', tag: 'input' });
+  input.setAttribute('name', 'name-input');
+  label.append(input);
+
+  // TODO add debounce
+  input.addListener('input', (e) => {
+    const target = e.target;
+    if (target instanceof HTMLInputElement) {
+      updateSearchByName(target.value);
+    }
+  });
+  return label;
+};
+
+export const clearNameSearch = () => {
+  const input = document.querySelector('.name-input');
+  if (!input) {
+    throw new Error('Input expected');
+  }
+  if (input instanceof HTMLInputElement) {
+    input.value = '';
+  }
+};
